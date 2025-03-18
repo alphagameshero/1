e4f0e1145083df0359f2353e5c7704f79990af7b
sos <69882754+LeeHwak2@users.noreply.github.com>
2025-03-18 02:59:30 +0000

    Update config.php

--- config.php
+++ config.php
@@ -11,21 +11,18 @@ alpha games
 
 //More Apps: App(1)
 Join our Discord Server
-https://storage.googleapis.com/sln_images/discord.png
-https://discord.gg/V2DuTHCC
-
+https://play-lh.googleusercontent.com/PLf6OjopS4TJ4sulGPIVXNQq2bfP7lMI2fJ757_vhzFALxCMhXEGf-I8Mt5UFKmgcQ=w240-h480
+https://play.google.com/store/apps/details?id=com.ppa.winter.shooting.strike.heros
 
 //More Apps: App(1)
 Sprunki: Cute vs Scary Beats
-https://storage.googleapis.com/sln_images/cutevsscarybeats.jpg
-market://details?id=com.slngames.sprunki
-
+https://play-lh.googleusercontent.com/PLf6OjopS4TJ4sulGPIVXNQq2bfP7lMI2fJ757_vhzFALxCMhXEGf-I8Mt5UFKmgcQ=w240-h480
+https://play.google.com/store/apps/details?id=com.ppa.winter.shooting.strike.heros
 
 //More Apps: App(1)
 Piggy Intercity: Siren Head Mod
-https://storage.googleapis.com/sln_images/intercitymod2.jpg
-market://details?id=com.intercity.piggy.sirenhead.mod
-
+https://play-lh.googleusercontent.com/PLf6OjopS4TJ4sulGPIVXNQq2bfP7lMI2fJ757_vhzFALxCMhXEGf-I8Mt5UFKmgcQ=w240-h480
+https://play.google.com/store/apps/details?id=com.ppa.winter.shooting.strike.heros
 
 //ads timer
 30
