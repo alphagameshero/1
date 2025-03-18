e1fa434dcd55b1bbf56e46e2e47dd915deb87dee
sos <69882754+LeeHwak2@users.noreply.github.com>
2025-03-18 02:50:02 +0000

    Create config.php

---
+++ config.php
@@ -0,0 +1,34 @@
+//Show Ads? (true/false)
+true
+
+//Show Featured Games? (true/false)
+false
+
+
+//Featured Game Comment:
+alpha games
+
+
+//More Apps: App(1)
+Join our Discord Server
+https://storage.googleapis.com/sln_images/discord.png
+https://discord.gg/V2DuTHCC
+
+
+//More Apps: App(1)
+Sprunki: Cute vs Scary Beats
+https://storage.googleapis.com/sln_images/cutevsscarybeats.jpg
+market://details?id=com.slngames.sprunki
+
+
+//More Apps: App(1)
+Piggy Intercity: Siren Head Mod
+https://storage.googleapis.com/sln_images/intercitymod2.jpg
+market://details?id=com.intercity.piggy.sirenhead.mod
+
+
+//ads timer
+30
+
+//show og mode
+false
